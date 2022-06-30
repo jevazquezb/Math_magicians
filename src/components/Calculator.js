248fec9358a627672b4502edac620cbc327c92a2
jevazquezb <je.vazquezb@gmail.com>
2022-06-30 18:00:19 -0500

    Refactor class based component to a functional component with hooks in Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class CalcUI extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+function CalcUI() {
+  const [stateObj, setStateObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  updateState = (e) => {
+  const updateState = (e) => {
     const key = e.target.textContent;
-    this.setState((state) => calculate(state, key));
-  }
+    setStateObj((state) => calculate(state, key));
+  };
 
-  display = (obj) => obj.next || obj.total || 0;
+  const display = (obj) => obj.next || obj.total || 0;
 
-  createDigits = () => {
-    const numsOps = ['AC', '+/\u2212', '%', '\xF7', 7, 8, 9, '\xD7', 4, 5, 6, '\u2212', 1, 2, 3, '+', 0, '.', '='];
+  const createDigits = () => {
+    const numsOps = [
+      'AC', '+/\u2212', '%', '\xF7',
+      7, 8, 9, '\xD7',
+      4, 5, 6, '\u2212',
+      1, 2, 3, '+',
+      0, '.', '=',
+    ];
     const digits = [];
-
     let classes = '';
     for (let i = 0; i < numsOps.length; i += 1) {
       if ((i + 1) % 4 === 0 || numsOps[i] === '=') {
@@ -32,22 +34,20 @@ class CalcUI extends React.PureComponent {
         classes = 'btn';
       }
       digits.push(
-        <button className={classes} onClick={this.updateState} type="button" key={numsOps[i]}>{numsOps[i]}</button>,
+        <button className={classes} onClick={updateState} type="button" key={numsOps[i]}>{numsOps[i]}</button>,
       );
     }
     return digits;
-  }
+  };
 
-  render() {
-    return (
-      <div className="calc-cont">
-        <div className="calc-disp">{ this.display(this.state) }</div>
-        <div className="btn-cont">
-          { this.createDigits() }
-        </div>
+  return (
+    <div className="calc-cont">
+      <div className="calc-disp">{ display(stateObj) }</div>
+      <div className="btn-cont">
+        { createDigits() }
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default CalcUI;
